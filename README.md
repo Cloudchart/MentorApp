12de5e99f61171ed45a38792185749451e6aebf0
Denis Izmaylov <izmaylov.dm@gmail.com>
2016-03-18 21:50:59 +0300

    Refactor Subscription scene. Add in-app purchase feature. Update README.

--- README.md
+++ README.md
@@ -5,24 +5,38 @@ Mentor2 App
 
 1. [Installation](#installation)
 2. [Build](#build)
+3. [How to Test In-app Purchases](#how-to-test-in-app-purchases)
 
 ## Installation
 
-Run in the project directory:
+1. Run in the project directory:
 
-```bash
-npm i
-cd ios
-sudo gem install cocoapods
-pod install
-```
+  ```bash
+  npm i
+  npm install rnpm -g
+  rnpm link react-native-in-app-utils
+  cd ios
+  sudo gem install cocoapods
+  pod install
+  ```
+  
+2. Now you have to set up your in-app purchases in iTunes Connect. Follow [this tutorial](http://stackoverflow.com/questions/19556336/how-do-you-add-an-in-app-purchase-to-an-ios-application) for easy explanation.
   
 ## Build
 
 1. Open XCode.
 2. Run in the simulator: `cmd + R` 
  
+## How to Test In-app Purchases
 
+For testing your in-app purchases you should *run the app on an actual device*. Using the iOS Simulator will always fail.
 
+1. Set up a test account ("Sandbox Tester") in iTunes Connect. See the official documentation [here](https://developer.apple.com/library/ios/documentation/LanguagesUtilities/Conceptual/iTunesConnect_Guide/Chapters/SettingUpUserAccounts.html#//apple_ref/doc/uid/TP40011225-CH25-SW9).
 
- 
+2. Run your app on an actual iOS device:
+
+  + [Run the react-native server on the local network](https://facebook.github.io/react-native/docs/runningondevice.html) instead of localhost. 
+  + Connect your device to Mac via USB
+  + [Select it from the list of available devices and simulators](https://i.imgur.com/6ifsu8Q.jpg) in the top bar (it's next to the build and stop buttons).
+
+3. Open the app and buy something with your Sandbox Tester Apple Account!
---
+++ app/scenes/subscription/index.js
@@ -0,0 +1,183 @@
+import { NativeModules } from 'react-native'
+import { InAppUtils } from 'NativeModules'
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+  AlertIOS,
+} from 'react-native'
+import styles from './styles'
+
+const PRODUCT_3_PREMIUM_TOPICS = 'org.reactjs.native.example.Mentor2.3_premium_topics';
+const PRODUCT_FULL_ACCESS_MONTHLY = 'org.reactjs.native.example.Mentor2.full_access_monthly';
+const PRODUCT_FULL_ACCESS_YEARLY = 'org.reactjs.native.example.Mentor2.full_access_yearly';
+
+const ActiveOpacity = {
+  activeOpacity: 0.75
+};
+
+class Subscription extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      loadedProducts: {}
+    };
+  }
+
+  componentDidMount() {
+    const products = [
+      PRODUCT_3_PREMIUM_TOPICS,
+      PRODUCT_FULL_ACCESS_MONTHLY,
+      PRODUCT_FULL_ACCESS_YEARLY,
+    ];
+    InAppUtils.loadProducts(products, (error, products) => {
+      if (error) {
+        alert(
+          'Failed to load products. It\'s OK if you run the app in Simulator. ' +
+          'Mock data will be used to display products list.'
+        );
+        const productsMock = {};
+        const prices = [0.99, 7.99, 5.60];
+        products.forEach((value, index) => {
+          productsMock[value] = {
+            identifier: value,
+            price: prices[index],
+            currencySymbol: '$',
+            currencyCode: 'USD',
+            priceString: '$' + prices[index],
+            downloadable: false,
+            description: 'Description for ' + value,
+            title: 'Title for ' + value
+          }
+        });
+        this.setState({
+          loadedProducts: productsMock
+        });
+        return;
+      } // if (error)
+
+      const loadedProducts = {};
+      products.forEach(value => {
+        loadedProducts[value.identifier] = value;
+      });
+      this.setState({ loadedProducts });
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={ styles.container }>
+          <Text style={ styles.noteText }>
+            Subscribe for the most efficient mentoring:
+            get more advices, more topic slots,
+            premium content, detailed progress state.
+          </Text>
+          <View style={ styles.subscriptionsContainer }>
+            <TouchableOpacity
+              {...ActiveOpacity}
+              style={ styles.subscription }
+              onPress={() => this.handlePurchaseClick(PRODUCT_3_PREMIUM_TOPICS)}
+              >
+              <View style={ styles.subscriptionTitle }>
+                <Text style={ styles.subscriptionTitleText }>
+                  3 premium topics
+                </Text>
+                <Text style={ styles.subscriptionTitleNote }>
+                  incl. this one
+                </Text>
+              </View>
+              <View style={ styles.subscriptionPrice }>
+                <Text style={ styles.subscriptionPriceText }>
+                  <Text style={ styles.subscriptionPriceTextNote }>just</Text>
+                  <Text>&nbsp;</Text>
+                  $0.99
+                  <Text style={ [styles.subscriptionPriceTextNote, styles.transparentText] }>/mo</Text>
+                </Text>
+                <Text style={ styles.subscriptionTitleNote }/>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity {...ActiveOpacity}
+              style={ styles.subscription }
+              onPress={() => this.handlePurchaseClick(PRODUCT_FULL_ACCESS_MONTHLY)}
+              >
+              <View style={ styles.subscriptionTitle }>
+                <Text style={ styles.subscriptionTitleText }>
+                  Full access
+                </Text>
+                <Text style={ styles.subscriptionTitleNote }>
+                  (billed monthly)
+                </Text>
+              </View>
+              <View style={ styles.subscriptionPrice }>
+                <Text style={ styles.subscriptionPriceText }>
+                  $7.99
+                  <Text style={ styles.subscriptionPriceTextNote }>/mo</Text>
+                </Text>
+                <Text style={ styles.subscriptionTitleNote }/>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              {...ActiveOpacity}
+              style={styles.subscription}
+              onPress={() => this.handlePurchaseClick(PRODUCT_FULL_ACCESS_YEARLY)}
+              >
+              <View style={ styles.subscriptionTitle }>
+                <Text style={ styles.subscriptionTitleText }>
+                  Full access
+                </Text>
+                <Text style={ styles.subscriptionTitleNote }>
+                  (billed annually*)
+                </Text>
+              </View>
+              <View style={ styles.subscriptionPrice }>
+                <Text style={ [styles.subscriptionPriceText, styles.subscriptionDisabledText] }>
+                  $7.99
+                </Text>
+                <Text style={ styles.subscriptionTitleNote }/>
+                <View style={ styles.deleted }/>
+              </View>
+              <View style={ styles.subscriptionPrice }>
+                <Text style={ styles.subscriptionPriceText }>
+                  $5.60
+                  <Text style={ styles.subscriptionPriceTextNote }>/mo</Text>
+                </Text>
+                <Text style={ styles.subscriptionTitleNote }/>
+              </View>
+            </TouchableOpacity>
+
+            <View style={ styles.subscriptionsFootnote}>
+              <Text style={ styles.subscriptionsFootnoteText }>
+                * 30% off! That's mentoring for just 18 cents
+              </Text>
+              <Text style={ styles.subscriptionsFootnoteText }>
+                a day or the price of 1 coffee in 3 weeks
+              </Text>
+            </View>
+          </View>
+
+          <TouchableOpacity { ...ActiveOpacity } style={ styles.denyControl }>
+            <Text style={ styles.denyControlText}>Sorry, not now</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    )
+  }
+
+  handlePurchaseClick(productID) {
+    InAppUtils.purchaseProduct(productID, (error, response) => {
+      if (error) {
+        AlertIOS.alert('Error', 'Failed to purchase. Please contact Application Developer.');
+        return;
+      }
+      if (response && response.productIdentifier) {
+        alert('addCredit', JSON.stringify(response));
+      }
+    });
+  }
+}
+
+export default Subscription
